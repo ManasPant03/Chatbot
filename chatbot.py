dadaebb41ff8e7f404443c4c89f3f0f32d243787
Manas Pant <manaspant72@gmail.com>
2024-10-27 03:28:07 +0530

    Updating

--- chatbot.py
+++ chatbot.py
@@ -3,16 +3,16 @@ import json
 import pickle
 import numpy as np
 import nltk
+import tensorflow as tf
 
 from nltk.stem import WordNetLemmatizer
-from keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbot_model.h5')
+model = tf.keras.models.load_model('chatbot_model.h5')
 
 
 def clean_up_sentence(sentence):
